e4ff11a5f4174214c29779d5caa1a0b99ca0d6ab
komed3 <pk97@outlook.de>
2025-12-02 19:37:43 +0100

    load / save json files

--- src/core/Storage.ts
+++ src/core/Storage.ts
@@ -1,3 +1,7 @@
+import { existsSync } from 'node:fs';
+import { readFile, writeFile } from 'node:fs/promises';
+import {  } from 'node:path';
+
 export class Storage {
 
     private static instance: Storage;
@@ -8,6 +12,22 @@ export class Storage {
 
         if ( ! Storage.instance ) Storage.instance = new Storage();
         return Storage.instance;
+
+    }
+
+    private async saveJson< T = any > ( filePath: string, data: T ) : Promise< void > {
+
+        const jsonData = JSON.stringify( data, null, 2 );
+        await writeFile( filePath, jsonData, 'utf8' );
+
+    }
+
+    private async loadJson< T = any > ( filePath: string ) : Promise< T | false > {
+
+        if ( ! existsSync( filePath ) ) return false;
+
+        const data = await readFile( filePath, 'utf8' );
+        return JSON.parse( data ) as T;
 
     }
 
