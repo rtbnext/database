d4beb7a0bfa85427fe7f44ad8382cbf4fb62f90d
komed3 <pk97@outlook.de>
2025-12-02 19:25:27 +0100

    Create Storage.ts

---
+++ src/core/Storage.ts
@@ -0,0 +1,14 @@
+export class Storage {
+
+    private static instance: Storage;
+
+    private constructor () {}
+
+    public static getInstance () : Storage {
+
+        if ( ! Storage.instance ) Storage.instance = new Storage();
+        return Storage.instance;
+
+    }
+
+}
