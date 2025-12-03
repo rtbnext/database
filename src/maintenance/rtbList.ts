195738dcb082781ed84b4af92c4182b056446878
komed3 <pk97@outlook.de>
2025-12-03 19:17:38 +0100

    define RTB response

--- src/maintenance/rtbList.ts
+++ src/maintenance/rtbList.ts
@@ -1,14 +1,55 @@
 import { Maintenance } from '../core/Maintenance';
 
+export interface RTBResponse {
+    personList: {
+        personsLists: {
+            uri: string;
+            rank: number;
+            finalWorth: number;
+            person: {
+                name: string;
+            };
+            personName: string;
+            state?: string;
+            city?: string;
+            source?: string;
+            industries: string[];
+            countryOfCitizenship: string;
+            gender: string;
+            birthDate: number;
+            lastName: string;
+            financialAssets: {
+                exchange: string;
+                ticker: string;
+                companyName: string;
+                numberOfShares?: number;
+                sharePrice: number;
+                currencyCode: string;
+                exchangeRate?: number;
+            }[];
+            familyList: boolean;
+            bios: string[];
+            abouts: string[];
+        }[];
+    }
+};
+
 class RTBList extends Maintenance {
 
     public async run () : Promise< void > {
 
         this.logger.info( `Fetch daily real-time billionaires list` );
 
-        const res = await this.fetch.request( this.config.endpoints.list.replace( '{URI}', 'rtb' ) );
+        const res = await this.fetch.request< RTBResponse >(
+            this.config.endpoints.list.replace( '{URI}', 'rtb' )
+        );
 
-        //
+        if ( ! res.success || ! res.data ) { this.logger.error( res.error || '' ) } else {
+
+            const list = res.data?.personList?.personsLists ?? [];
+            // proceed list data ...
+
+        }
 
     }
 
