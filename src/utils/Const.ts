1b95c54d0e43599726fbc6a20f06714a65912370
komed3 <pk97@outlook.de>
2025-12-10 23:48:13 +0100

    define marital status

--- src/utils/Const.ts
+++ src/utils/Const.ts
@@ -1,2 +1,8 @@
-export const Gender = [ 'm', 'f', 'd' ] as const;
 export type Gender = ( typeof Gender )[ number ];
+export const Gender = [ 'm', 'f', 'd' ] as const;
+
+export type MaritalStatus = ( typeof MaritalStatus )[ number ];
+export const MaritalStatus = [
+    'single', 'relationship', 'married', 'remarried', 'engaged',
+    'separated', 'divorced', 'widowed'
+] as const;
