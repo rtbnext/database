b856e53233335f3c421d6d2bf4a5d3552ccec991
komed3 <pk97@outlook.de>
2025-12-10 19:21:45 +0100

    parsing primitives and lists

--- src/utils/Parser.ts
+++ src/utils/Parser.ts
@@ -1,3 +1,5 @@
+import { Primitive } from 'devtypes/types/primitives';
+
 export class Parser {
 
     public static string ( value: any ) : string {
@@ -15,6 +17,18 @@ export class Parser {
 
     public static number ( value: any, digits: number = 0 ) : number {
         return Number( Number( value ).toFixed( digits ) );
+    }
+
+    public static primitive ( value: any ) : Primitive {
+        return value === null || value === undefined ? value
+            : typeof value === 'boolean' ? value
+                : ! isNaN( Number( value ) ) && value !== '' ? this.number( value )
+                    : this.string( value );
+    }
+
+    public static list ( value: any, delimiter: string = ',' ) : Primitive[] {
+        const list = Array.isArray( value ) ? value : value.split( delimiter );
+        return list.map( this.primitive ).filter( Boolean );
     }
 
     public static money ( value: any ) : number {
