5f930cf24346cd4593fccababa14edea9833b425
komed3 <pk97@outlook.de>
2025-12-10 23:59:18 +0100

    define industry const

--- src/utils/Const.ts
+++ src/utils/Const.ts
@@ -6,3 +6,10 @@ export const MaritalStatus = [
     'single', 'relationship', 'married', 'remarried', 'engaged',
     'separated', 'divorced', 'widowed'
 ] as const;
+
+export type Industry = ( typeof Industry )[ number ];
+export const Industry = [
+    'automotive', 'diversified', 'energy', 'engineering', 'finance', 'foodstuff',
+    'gambling', 'healthcare', 'logistics', 'manufacturing', 'media', 'mining',
+    'property', 'retail', 'service', 'sports', 'technology', 'telecom'
+] as const;
