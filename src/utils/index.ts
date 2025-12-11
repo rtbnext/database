8870e635604e814da12b94395a0335c7ca0438e9
komed3 <pk97@outlook.de>
2025-12-11 02:11:09 +0100

    add ConfigLoader class

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,13 @@
 import { Base } from './Base';
+import { ConfigLoader } from './ConfigLoader';
 import * as Const from './Const';
 import { Logger } from './Logger';
 import { Parser } from './Parser';
 
 const Utils = {
-    const: Const,
-    base: Base,
-    logger: Logger,
-    parser: Parser
+    const: Const, base: Base, config: ConfigLoader,
+    logger: Logger, parser: Parser
 };
 
 export default Utils;
-export { Const, Base, Logger, Parser };
+export { Const, Base, ConfigLoader, Logger, Parser };
