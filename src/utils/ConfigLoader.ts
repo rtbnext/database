4b880b865ff2ca074fcf19189f1b723faa57d028
komed3 <pk97@outlook.de>
2025-12-11 02:55:41 +0100

    load config file

--- src/utils/ConfigLoader.ts
+++ src/utils/ConfigLoader.ts
@@ -1,14 +1,30 @@
+import { existsSync, readFileSync } from 'node:fs';
 import { Config, LoggingConfig } from '../types/config';
+import { join } from 'node:path';
+import { cwd } from 'node:process';
+import { parse } from 'yaml';
 
 export class ConfigLoader {
 
     private static instance: ConfigLoader;
+    private readonly cwd: string;
+    private readonly path: string;
     private readonly env: string;
     private readonly cfg: Config;
 
     private constructor () {
+        this.cwd = cwd();
+        this.path = join( this.cwd, 'config' );
         this.env = process.env.NODE_ENV || 'production';
-        this.cfg = {} as Config;
+        this.cfg = this.loadConfigFile();
+    }
+
+    private loadConfigFile () : Config {
+        [ `${ this.env }.yml`, 'default.yml' ].forEach( file => {
+            const path = join( this.path, file );
+            if ( existsSync( path ) ) return parse( readFileSync( path, 'utf8' ) ) as Config;
+        } );
+        throw new Error( 'No configuration file found' );
     }
 
     public get environment () : string { return this.env }
