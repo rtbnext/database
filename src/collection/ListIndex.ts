6b9c95b70d2600e7da44e330cc1edc99b677cd81
komed3 <pk97@outlook.de>
2025-12-16 23:26:35 +0100

    instantiating index classes

--- src/collection/ListIndex.ts
+++ src/collection/ListIndex.ts
@@ -1,3 +1,15 @@
 import { Index } from '@/abstract/Index';
 
-export class ListIndex extends Index {}
+export class ListIndex extends Index {
+
+    protected static instance: ListIndex;
+
+    private constructor () {
+        super();
+    }
+
+    public static getInstance () {
+        return ListIndex.instance ||= new ListIndex();
+    }
+
+}
--- src/collection/ProfileIndex.ts
+++ src/collection/ProfileIndex.ts
@@ -1,3 +1,15 @@
 import { Index } from '@/abstract/Index';
 
-export class ProfileIndex extends Index {}
+export class ProfileIndex extends Index {
+
+    protected static instance: ProfileIndex;
+
+    private constructor () {
+        super();
+    }
+
+    public static getInstance () {
+        return ProfileIndex.instance ||= new ProfileIndex();
+    }
+
+}
