79aee58418832a1781caf2ff3183c01fb4d7c095
komed3 <pk97@outlook.de>
2025-12-16 23:48:37 +0100

    Update ProfileIndex.ts

--- src/collection/ProfileIndex.ts
+++ src/collection/ProfileIndex.ts
@@ -10,9 +10,9 @@ export class ProfileIndex extends Index< TProfileIndex > {
     }
 
     protected loadIndex () : TProfileIndex {
-            const raw = this.storage.readJSON< Record< string, TProfileIndexItem > > ( 'profile/index.json' ) || {};
-            return new Map( Object.entries( raw ) );
-        }
+        const raw = this.storage.readJSON< Record< string, TProfileIndexItem > > ( 'profile/index.json' ) || {};
+        return new Map( Object.entries( raw ) );
+    }
 
     public static getInstance () {
         return ProfileIndex.instance ||= new ProfileIndex();
