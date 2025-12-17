1d75eb39616bc1f338233a3685c39cf114e2a3bf
komed3 <pk97@outlook.de>
2025-12-17 11:43:55 +0100

    add generic types

--- src/types/generic.d.ts
+++ src/types/generic.d.ts
@@ -2,3 +2,19 @@ export interface TMetaData {
     schemaVersion: 2,
     lastModified: string;
 };
+
+export type TLocation = {
+    country: string;
+    state?: string;
+    city?: string;
+};
+
+export type TImage = {
+    url: string;
+    credits: string;
+    file: string;
+    thumb?: string;
+    caption?: string;
+    desc?: string;
+    date?: string;
+};
