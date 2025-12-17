5862fdf86851cdac554065dda6162d69c16ca58b
komed3 <pk97@outlook.de>
2025-12-17 11:31:29 +0100

    describe profile types

--- src/types/profile.d.ts
+++ src/types/profile.d.ts
@@ -1,3 +1,5 @@
+import { TMetaData } from '@/types/generic';
+
 export type TProfileIndex< T extends string = string > = Map< T, TProfileIndexItem< T > >;
 
 export interface TProfileIndexItem< T extends string = string > {
@@ -5,4 +7,18 @@ export interface TProfileIndexItem< T extends string = string > {
     name: string;
     aliases: string[];
     text: string;
+};
+
+export interface TProfileData< T extends string = string > {
+    uri: T;
+};
+
+export type TProfileHistory = TProfileHistoryItem[];
+
+export type TProfileHistoryItem = [ string, number, number, number, number ];
+
+export type TProfile< T extends string = string > = {
+    meta: TMetaData;
+    profile: TProfileData< T >;
+    history: TProfileHistory;
 };
