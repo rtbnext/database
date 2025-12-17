2902817e3d307eab7529e9fd87dfa854eed6c14e
komed3 <pk97@outlook.de>
2025-12-17 11:33:26 +0100

    profile data parser

--- src/collection/Profile.ts
+++ src/collection/Profile.ts
@@ -1,1 +1,10 @@
-export class Profile {}
+import { TProfileData } from '@/types/profile';
+import { TProfileResponse } from '@/types/response';
+
+export class Profile {
+
+    public static parser ( raw: TProfileResponse ) : Partial< TProfileData > {
+        return {};
+    }
+
+}
