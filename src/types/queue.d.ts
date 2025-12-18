947cd05b0c68fc30521a06e4479b6bbdd94ade8d
komed3 <pk97@outlook.de>
2025-12-18 16:31:18 +0100

    Update queue.d.ts

--- src/types/queue.d.ts
+++ src/types/queue.d.ts
@@ -1,10 +1,12 @@
-export interface TQueue {
-    profile: TQueueItem[];
-    list: TQueueItem[];
-}
+export const QueueType = [ 'profile', 'list' ] as const;
+export type QueueType = ( typeof QueueType )[ number ];
 
 export interface TQueueItem {
     uri: string;
     ts: string;
     prio?: number;
 }
+
+export type TQueue = { [ K in QueueType ]: Map< string, TQueueItem > };
+
+export type TQueueStorage = { [ K in QueueType ]: TQueueItem[] };
