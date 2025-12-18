2c61b760c548194743b9f959f3473ef774998e2e
komed3 <pk97@outlook.de>
2025-12-18 18:19:58 +0100

    Update FetchProfile.ts

--- src/job/fetchProfile.ts
+++ src/job/fetchProfile.ts
@@ -1,29 +1,27 @@
 import { Job, jobRunner } from '@/abstract/Job';
 import { ProfileParser } from '@/collection/Profile';
+import { TArgs } from '@/types/generic';
 
 export class FetchProfile extends Job {
 
     protected override readonly job = 'FetchProfile';
 
-    public async run () : Promise< void > {
-        const batch = this.queue.next( 'profile', this.config.fetch.rateLimit.maxBatchSize );
-        const res = await this.fetch.profile( ...batch.map( i => i.uri ) );
+    public async run ( args: TArgs ) : Promise< void > {
+        const batch = 'profile' in args && typeof args.profile === 'string'
+            ? args.profile.split( ',' ).filter( Boolean ).map( i => ( { uri: i } ) )
+            : this.queue.next( 'profile', this.config.fetch.rateLimit.maxBatchSize );
 
-        for ( const row of res ) {
+        for ( const row of await this.fetch.profile( ...batch.map( i => i.uri ) ) ) {
             if ( ! row || ! row.success || ! row.data ) {
                 this.log( 'Request failed', row, 'warn' );
                 continue;
             }
 
             const parser = new ProfileParser( row.data );
+            // create or update profile
         }
-    }
-
-    public async single () : Promise< void > {
-        //
     }
 
 }
 
 jobRunner( FetchProfile );
-jobRunner( FetchProfile, 'single', '--profile' );
