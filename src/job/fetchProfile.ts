9c32cfc253ce8ef649b85deae6599c104be34fea
komed3 <pk97@outlook.de>
2025-12-18 17:42:54 +0100

    loop through fetched profiles and parse data

--- src/job/fetchProfile.ts
+++ src/job/fetchProfile.ts
@@ -1,12 +1,29 @@
 import { Job, jobRunner } from '@/abstract/Job';
+import { ProfileParser } from '@/collection/Profile';
 
 export class FetchProfile extends Job {
 
+    protected override readonly job = 'FetchProfile';
+
     public async run () : Promise< void > {
         const batch = this.queue.next( 'profile', this.config.fetch.rateLimit.maxBatchSize );
-        const res = this.fetch.profile( ...batch.map( i => i.uri ) );
+        const res = await this.fetch.profile( ...batch.map( i => i.uri ) );
+
+        for ( const row of res ) {
+            if ( ! row || ! row.success || ! row.data ) {
+                this.log( 'Request failed', row, 'warn' );
+                continue;
+            }
+
+            const parser = new ProfileParser( row.data );
+        }
+    }
+
+    public async single () : Promise< void > {
+        //
     }
 
 }
 
 jobRunner( FetchProfile );
+jobRunner( FetchProfile, 'single', '--profile' );
