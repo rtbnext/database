fd600acae176ebe91378c48307d79a3d391e8828
komed3 <pk97@outlook.de>
2025-12-19 12:14:59 +0100

    resolve path aliases during build

--- rollup.config.js
+++ rollup.config.js
@@ -31,7 +31,7 @@ export default {
             singleQuote: true,
             jsxSingleQuote: true,
             trailingComma: 'none',
-            objectWrap: 'collapse',
+            objectWrap: 'collapse'
         } )
     ]
 };
