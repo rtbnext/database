c44d7ea08b67f4748eeb5cb3b71b6edb5497419b
komed3 <pk97@outlook.de>
2025-12-19 20:22:59 +0100

    prep. CmpStr library

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -1,12 +1,22 @@
-import { Profile } from '@/collection';
+import { Profile } from '@/collection/Profile';
 import { ProfileIndex } from '@/collection/ProfileIndex';
 import { TProfileData } from '@/types/profile';
-import { CmpStr } from 'cmpstr';
+import { CmpStr, CmpStrResult } from 'cmpstr';
+
+CmpStr.filter.add( 'input', 'normalizeUri', ( uri: string ) : string =>
+    uri.replace( /-(family|\d+)$/i, '' )
+);
 
 export class ProfileMerger {
 
-    private static readonly cmp = CmpStr.create( { metric: 'levenshtein' } );
+    private static readonly cmp = CmpStr.create( { metric: 'dice' } );
     private static readonly index = ProfileIndex.getInstance();
+
+    private static findSimilar ( uri: string ) : string[] {
+        return ProfileMerger.cmp.match< CmpStrResult[] >(
+            [ ...ProfileMerger.index.getIndex().keys() ], uri, 0.6
+        ).map( i => i.source );
+    }
 
     public static mergeProfiles ( target: Profile, source: Profile ) : void {}
 
