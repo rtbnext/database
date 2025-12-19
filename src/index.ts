98a5108b5ac68ebcd794a5682e62d62d3e0d2d6a
komed3 <pk97@outlook.de>
2025-12-19 01:37:33 +0100

    create root index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { UpdateProfile } from '@/job/UpdateProfile';
+
+export const jobs = { UpdateProfile } as const;
+
+export * from '@/collection';
+export * from '@/core';
+export * from '@/utils';
