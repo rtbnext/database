23b039a95d92a9c0b8d631adc4b44c083124a81e
komed3 <pk97@outlook.de>
2025-12-19 19:51:13 +0100

    implement public methods for profile merger

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -1,1 +1,15 @@
-export class ProfileMerger {}
+import { Profile } from '@/collection';
+import { ProfileIndex } from '@/collection/ProfileIndex';
+import { TProfileData } from '@/types/profile';
+import { CmpStr } from 'cmpstr';
+
+export class ProfileMerger {
+
+    private static readonly cmp = CmpStr.create( { metric: 'levenshtein' } );
+    private static readonly index = ProfileIndex.getInstance();
+
+    public static mergeProfiles ( target: Profile, source: Profile ) : void {}
+
+    public static findMatch ( data: Partial< TProfileData > ) : Profile | false {}
+
+}
