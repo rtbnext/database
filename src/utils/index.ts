63f2deba4eb62b069f79a11ef68a533d65f2b38b
komed3 <pk97@outlook.de>
2025-12-19 19:41:16 +0100

    Create ProfileMerger.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,17 @@
 import * as Const from '@/utils/Const';
 import { Logger } from '@/utils/Logger';
 import { Parser } from '@/utils/Parser';
+import { ProfileMerger } from '@/utils/ProfileMerger';
 import { ProfileParser } from '@/utils/ProfileParser';
 import { Utils } from '@/utils/Utils';
 
 const helper = {
     log: Logger.getInstance(),
     parser: Parser,
+    profileMerger: ProfileMerger,
     profileParser: ProfileParser,
     utils: Utils
 } as const;
 
-export { Const, Logger, Parser, ProfileParser, Utils };
+export { Const, Logger, Parser, ProfileMerger, ProfileParser, Utils };
 export default helper;
