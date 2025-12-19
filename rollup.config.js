253ac9b6e9f95bc5e75cd8541e217808262ebb0c
komed3 <pk97@outlook.de>
2025-12-19 01:39:42 +0100

    setup rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,36 @@
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import prettier from 'rollup-plugin-prettier';
+
+export default {
+    input: [ 'src/index.ts' ],
+    output: [ {
+        dir: 'dist',
+        format: 'esm',
+        entryFileNames: '[name].js',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        sourcemap: true
+    } ],
+    plugins: [
+        resolve(),
+        typescript( {
+            tsconfig: './tsconfig.json'
+        } ),
+        terser( {
+            format: { comments: false },
+            compress: false
+        } ),
+        prettier( {
+            parser: 'babel',
+            tabWidth: 2,
+            bracketSpacing: true,
+            bracketSameLine: true,
+            singleQuote: true,
+            jsxSingleQuote: true,
+            trailingComma: 'none',
+            objectWrap: 'collapse',
+        } )
+    ]
+};
