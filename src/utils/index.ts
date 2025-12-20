7d040a1a135520080d9d2e1977c01428d3780115
komed3 <pk97@outlook.de>
2025-12-20 11:02:11 +0100

    Create utils/Wiki.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,14 +4,16 @@ import { Parser } from '@/utils/Parser';
 import { ProfileMerger } from '@/utils/ProfileMerger';
 import { ProfileParser } from '@/utils/ProfileParser';
 import { Utils } from '@/utils/Utils';
+import { Wiki } from '@/utils/Wiki';
 
 const helper = {
     log: Logger.getInstance(),
     parser: Parser,
     profileMerger: ProfileMerger,
     profileParser: ProfileParser,
-    utils: Utils
+    utils: Utils,
+    wiki: Wiki
 } as const;
 
-export { Const, Logger, Parser, ProfileMerger, ProfileParser, Utils };
+export { Const, Logger, Parser, ProfileMerger, ProfileParser, Utils, Wiki };
 export default helper;
