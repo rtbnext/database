07429305e5a3e12a13538836393c823b0e38718d
komed3 <pk97@outlook.de>
2025-12-20 17:31:23 +0100

    add resolver objects for marital status & industry

--- src/utils/Const.ts
+++ src/utils/Const.ts
@@ -7,12 +7,45 @@ export const MaritalStatus = [
     'separated', 'divorced', 'widowed'
 ] as const;
 
+export type MaritalStatusResolver = Record< string, MaritalStatus >;
+export const MaritalStatusResolver = {
+    'single': 'single',
+    'in-relationship': 'relationship',
+    'married': 'married',
+    'remarried': 'remarried',
+    'engaged': 'engaged',
+    'separated': 'separated',
+    'divorced': 'divorced',
+    'widowed': 'widowed'
+} as const;
+
 export type Industry = ( typeof Industry )[ number ];
 export const Industry = [
     'automotive', 'diversified', 'energy', 'engineering', 'finance', 'foodstuff',
     'gambling', 'healthcare', 'logistics', 'manufacturing', 'media', 'mining',
     'property', 'retail', 'service', 'sports', 'technology', 'telecom'
 ] as const;
+
+export type IndustryResolver = Record< string, Industry >;
+export const IndustryResolver = {
+    'technology': 'technology',
+    'fashion-retail': 'retail',
+    'finance-investments': 'finance',
+    'diversified': 'diversified',
+    'telecom': 'telecom',
+    'energy': 'energy',
+    'metals-mining': 'mining',
+    'gambling-casinos': 'gambling',
+    'healthcare': 'healthcare',
+    'manufacturing': 'manufacturing',
+    'logistics': 'logistics',
+    'automotive': 'automotive',
+    'media-entertainment': 'media',
+    'construction-engineering': 'engineering',
+    'sports': 'sports',
+    'real-estate': 'property',
+    'service': 'service'
+} as const;
 
 export type Relationship = ( typeof Relationship )[ number ];
 export const Relationship = [ 'person', 'organization', 'place' ] as const;
