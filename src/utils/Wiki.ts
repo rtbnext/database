2ba0e1418bd8d8e19b7f28992e989442ee5d6d8e
komed3 <pk97@outlook.de>
2025-12-22 19:22:20 +0100

    Wiki class part 2/3 (implement querying WikiData)

--- src/utils/Wiki.ts
+++ src/utils/Wiki.ts
@@ -1,6 +1,8 @@
 import { Fetch } from '@/core/Fetch';
+import { TWikiData } from '@/types/generic';
 import { TProfileData } from '@/types/profile';
-import { TWikiDataResponseItem } from '@/types/response';
+import { TWikiDataResponse, TWikiDataResponseItem } from '@/types/response';
+import { Parser } from '@/utils/Parser';
 
 export class Wiki {
 
@@ -40,6 +42,54 @@ export class Wiki {
         if ( item.netWorth ) score += 0.2;
 
         return Math.min( 1, Math.max( 0, score ) );
+    }
+
+    public static async queryWikiData ( data: Partial< TProfileData > ) : Promise< TWikiData | undefined > {
+        const shortName = data.info?.shortName;
+        if ( ! shortName ) return;
+
+        const [ first, ...rest ] = shortName.split( ' ' ); const last = rest.pop();
+        const nameVariants = [ shortName, `${first[ 0 ]}. ${last}`, `${first} ${last}` ]
+            .filter( Boolean ).map( n => `"${n}"@en "${n}"@de` ).join( ' ' );
+
+        const sparql = `
+            SELECT DISTINCT
+                ?item ?itemLabel ?gender ?birthdate ?article ?image ?iso2
+                ?occupation ?employer ?ownerOf ?netWorth
+            WHERE {
+                VALUES ?name { ${nameVariants} }
+                ?item wdt:P31 wd:Q5 .
+                { { ?item rdfs:label ?name . } UNION { ?item skos:altLabel ?name . } }
+                OPTIONAL { ?item wdt:P21 ?gender . }
+                OPTIONAL { ?item wdt:P569 ?birthdate . }
+                OPTIONAL { ?article schema:about ?item ; schema:isPartOf <https://en.wikipedia.org/> . }
+                OPTIONAL { ?item wdt:P18 ?image . }
+                OPTIONAL { ?item wdt:P27 ?country . ?country wdt:P297 ?iso2 . }
+                OPTIONAL { ?item wdt:P106 ?occupation . }
+                OPTIONAL { ?item wdt:P108 ?employer . }
+                OPTIONAL { ?item wdt:P169 ?employer . }
+                OPTIONAL { ?item wdt:P127 ?ownerOf . }
+                OPTIONAL { ?item wdt:P1830 ?ownerOf . }
+                OPTIONAL { ?item wdt:P2218 ?netWorth . }
+                SERVICE wikibase:label { bd:serviceParam wikibase:language "en,de" . }
+            }
+            LIMIT 20
+        `;
+
+        const res = await Wiki.fetch.wikidata< TWikiDataResponse >( sparql );
+        let best: { score: number, item: TWikiDataResponseItem } | undefined;
+
+        for ( const item of res.data?.results.bindings ?? [] ) {
+            const score = Wiki.scoreWDItem( item, data );
+            if ( ! best || score > best.score ) best = { score, item };
+        }
+
+        if ( best && best.score >= 0.65 ) return Parser.container< TWikiData >( {
+            qid: { value: best.item.item.value.split( '/' ).pop()!, method: 'string' },
+            article: { value: best.item.article?.value.split( '/' ).pop(), method: 'decodeURI' },
+            image: { value: best.item.image?.value.split( '/' ).pop(), method: 'decodeURI' },
+            score: { value: best.score, method: 'number', args: [ 1 ] }
+        } );
     }
 
 }
