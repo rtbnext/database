dfcd7d709f82d884d5309b7299910a6f5ac3bea6
komed3 <pk97@outlook.de>
2025-12-22 14:45:18 +0100

    update exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,14 +1,4 @@
-import { Config } from '@/core/Config';
-import { Fetch } from '@/core/Fetch';
-import { Queue } from '@/core/Queue';
-import { Storage } from '@/core/Storage';
-
-const service = {
-    config: Config.getInstance,
-    fetch: Fetch.getInstance,
-    queue: Queue.getInstance,
-    storage: Storage.getInstance
-} as const;
-
-export { Config, Fetch, Queue, Storage };
-export default service;
+export { Config } from '@/core/Config';
+export { Fetch } from '@/core/Fetch';
+export { Queue } from '@/core/Queue';
+export { Storage } from '@/core/Storage';
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { MoveProfile } from '@/job/MoveProfile';
 import { UpdateProfile } from '@/job/UpdateProfile';
 import { UpdateWiki } from '@/job/UpdateWiki';
 
-export const jobs = { MoveProfile, UpdateProfile, UpdateWiki } as const;
+export const jobs = {
+    MoveProfile, UpdateProfile, UpdateWiki
+} as const;
 
 export * from '@/collection';
 export * from '@/core';
