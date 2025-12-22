d5b4aebf8701e46ff56b335e1953e498041d8f7d
komed3 <pk97@outlook.de>
2025-12-22 09:29:51 +0100

    implement update wiki job runner

--- src/job/UpdateWiki.ts
+++ src/job/UpdateWiki.ts
@@ -1,4 +1,5 @@
 import { Job, jobRunner } from '@/abstract/Job';
+import { Profile } from '@/collection';
 import { TArgs } from '@/types/generic';
 
 export class UpdateWiki extends Job {
@@ -7,7 +8,19 @@ export class UpdateWiki extends Job {
         super( silent, safeMode, 'UpdateWiki' );
     }
 
-    public async run ( args: TArgs ) : Promise< void > {}
+    private async update ( profile: Profile ) : Promise< void > {}
+
+    private async assign ( profile: Profile, title: string ) : Promise< void > {}
+
+    public async run ( args: TArgs ) : Promise< void > {
+        await this.protect( async () => {
+            const profile = Profile.find( ( args.profile ?? '' ) as string );
+            if ( ! profile ) throw new Error( `Profile not found: ${args.profile}` );
+
+            if ( args.check || args.update ) await this.update( profile );
+            else if ( typeof args.assign === 'string' ) await this.assign( profile, args.assign );
+        } );
+    }
 
 }
 
