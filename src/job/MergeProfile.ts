a6337fb2303e3351095c4070ce3eb55de9ee047d
komed3 <pk97@outlook.de>
2025-12-23 09:59:32 +0100

    list candidates

--- src/job/MergeProfile.ts
+++ src/job/MergeProfile.ts
@@ -1,5 +1,6 @@
 import { Job, jobRunner } from '@/abstract/Job';
 import { TArgs } from '@/types/generic';
+import { ProfileMerger } from '@/utils/ProfileMerger';
 
 export class MergeProfile extends Job {
 
@@ -7,14 +8,24 @@ export class MergeProfile extends Job {
         super( silent, safeMode, 'MergeProfile' );
     }
 
-    private listMergeable () : void {}
+    private listMergeable ( ...uriLike: any[] ) : void {
+        for ( const [ uri, candidates ] of Object.entries( ProfileMerger.listCandidates( uriLike ) ) ) {
+            console.log( `Candidates for ${uri}:` );
+            if ( ! candidates.length ) console.log( ' - None' );
+            for ( const candidate of candidates ) console.log( ` - ${candidate}` );
+        }
+    }
 
     private isMergeable () : void {}
 
-    private async merge () : Promise< void > {}
+    private merge () : void {}
 
     public async run ( args: TArgs ) : Promise< void > {
-        await this.protect( async () => {} );
+        await this.protect( async () => {
+            if ( typeof args.list === 'string' ) this.listMergeable(
+                ...args.list.split( ',' ).filter( Boolean )
+            )
+        } );
     }
 
 }
