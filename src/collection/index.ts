1d405478c5b8cdfced2fc59adfc5273ab9df05e1
komed3 <pk97@outlook.de>
2025-12-27 16:34:03 +0100

    export movers collection

--- src/collection/index.ts
+++ src/collection/index.ts
@@ -1,5 +1,6 @@
 export { List } from '@/collection/List';
 export { ListIndex } from '@/collection/ListIndex';
+export { Mover } from '@/collection/Mover';
 export { Profile } from '@/collection/Profile';
 export { ProfileIndex } from '@/collection/ProfileIndex';
 export { Stats } from '@/collection/Stats';
