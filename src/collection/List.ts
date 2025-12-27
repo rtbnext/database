df9d75b2e2ffd58625f07aa20f46b0c706651d2b
komed3 <pk97@outlook.de>
2025-12-27 17:00:01 +0100

    Update collection/List.ts

--- src/collection/List.ts
+++ src/collection/List.ts
@@ -25,9 +25,30 @@ export class List extends Dated< TListSnapshot > {
         return this.data;
     }
 
+    public saveSnapshot ( date: string, snapshot: TListSnapshot, force: boolean = false ) : boolean {
+        const res = super.saveSnapshot( date, snapshot, force );
+        if ( ! res || ! List.index.update( this.uri, {
+            date: snapshot.date,
+            count: snapshot.stats.count
+        } ) ) return false;
+
+        this.data.date = snapshot.date;
+        this.data.count = snapshot.stats.count;
+        this.dates = this.scanDates();
+
+        return true;
+    }
+
     public static get ( uriLike: string ) : List | false {
         try { return new List( List.index.get( uriLike ) ) }
         catch { return false }
+    }
+
+    public static create ( uriLike: any, data: TListIndexItem, snapshot?: TListSnapshot ) : List | false {
+        const item = List.index.add( uriLike, data ); if ( ! item ) return false;
+        const list = new List( item ); if ( ! list ) return false;
+        if ( snapshot ) list.saveSnapshot( snapshot.date, snapshot );
+        return list;
     }
 
 }
