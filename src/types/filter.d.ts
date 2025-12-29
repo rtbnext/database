8fc060616fc2298dc622e771623ed91c23780bc9
komed3 <pk97@outlook.de>
2025-12-29 09:41:40 +0100

    Update filter.d.ts

--- src/types/filter.d.ts
+++ src/types/filter.d.ts
@@ -5,13 +5,16 @@ export interface TFilter {
 
 export interface TFilterCollection {
     industry: Record< string, TFilter[] >;
+    citizenship: Record< string, TFilter[] >;
     country: Record< string, TFilter[] >;
     state: Record< string, TFilter[] >;
     gender: Record< string, TFilter[] >;
     age: Record< string, TFilter[] >;
+    maritalStatus: Record< string, TFilter[] >;
     special: {
         deceased: TFilter[];
         dropOff: TFilter[];
         family: TFilter[];
+        selfMade: TFilter[];
     };
 }
