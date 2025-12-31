9ce0240ab6fd064a6e7c47db1a433aa11f7e859f
komed3 <pk97@outlook.de>
2025-12-31 15:36:01 +0100

    restart final implementation

--- src/core/index.ts
+++
@@ -1,4 +0,0 @@
-export { Config } from '@/core/Config';
-export { Fetch } from '@/core/Fetch';
-export { Queue } from '@/core/Queue';
-export { Storage } from '@/core/Storage';
--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export * as Const from '@/utils/Const';
-export { ListParser } from '@/utils/ListParser';
-export { Parser } from '@/utils/Parser';
-export { ProfileMerger } from '@/utils/ProfileMerger';
-export { ProfileParser } from '@/utils/ProfileParser';
-export { Ranking } from '@/utils/Ranking';
-export { Utils } from '@/utils/Utils';
-export { Wiki } from '@/utils/Wiki';
