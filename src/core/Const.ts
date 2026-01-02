f1786bfb50fea0480a40429a11335245fec4bfde
komed3 <pk97@outlook.de>
2026-01-02 18:33:52 +0100

    min fix

--- src/core/Const.ts
+++ src/core/Const.ts
@@ -1,6 +1,8 @@
 import { TIndustryResolver, TMaritalStatusResolver } from '@/types/generic';
 import * as Const from '@rtbnext/schema/src/abstract/const';
 
+// Basics
+
 export const Gender: Const.TGender[] = [
     'm', 'f', 'd'
 ] as const;
@@ -15,6 +17,34 @@ export const Industry: Const.TIndustry[] = [
     'gambling', 'healthcare', 'logistics', 'manufacturing', 'media', 'mining',
     'property', 'retail', 'service', 'sports', 'technology', 'telecom'
 ] as const;
+
+export const ChangeFlag: Const.TChangeFlag[] = [
+    'up', 'down', 'unchanged'
+] as const;
+
+export const RelationType: Const.TRelationType[] = [
+    'person', 'organization', 'place', 'unknown'
+] as const;
+
+export const AssetType: Const.TAssetType[] = [
+    'public', 'private', 'misc'
+] as const;
+
+// Stats
+
+export const ChildrenGroup: Const.TChildrenGroup[] = [
+    'none', 'one', 'two', 'three', 'four', '5-to-10', 'over-10'
+] as const;
+
+export const Percentiles: Const.TPercentiles[] = [
+    '10th', '25th', '50th', '75th', '90th', '95th', '99th'
+] as const;
+
+export const WealthSpread: Const.TWealthSpread[] = [
+    '1', '2', '5', '10', '20', '50', '100', '200', '500'
+] as const;
+
+// Resolver
 
 export const MaritalStatusResolver: TMaritalStatusResolver = {
     'single': 'single',
@@ -46,27 +76,3 @@ export const IndustryResolver: TIndustryResolver = {
     'real-estate': 'property',
     'service': 'service'
 } as const;
-
-export const ChangeFlag: Const.TChangeFlag[] = [
-    'up', 'down', 'unchanged'
-] as const;
-
-export const RelationType: Const.TRelationType[] = [
-    'person', 'organization', 'place', 'unknown'
-] as const;
-
-export const AssetType: Const.TAssetType[] = [
-    'public', 'private', 'misc'
-] as const;
-
-export const ChildrenGroup: Const.TChildrenGroup[] = [
-    'none', 'one', 'two', 'three', 'four', '5-to-10', 'over-10'
-] as const;
-
-export const Percentiles: Const.TPercentiles[] = [
-    '10th', '25th', '50th', '75th', '90th', '95th', '99th'
-] as const;
-
-export const WealthSpread: Const.TWealthSpread[] = [
-    '1', '2', '5', '10', '20', '50', '100', '200', '500'
-] as const;
