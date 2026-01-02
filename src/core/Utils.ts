23bd8b1e467e3c304918e6b2124aeda9e5c7afb1
komed3 <pk97@outlook.de>
2026-01-02 10:07:10 +0100

    sanitize

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -1,1 +1,11 @@
-export class Utils {}
+export class Utils {
+
+    public static sanitize ( value: any, delimiter: string ) : string {
+        return String( value )
+            .toLowerCase()
+            .replace( /[^a-z0-9]/g, delimiter )
+            .replace( new RegExp( `[${delimiter}]{2,}`, 'g' ), delimiter )
+            .trim();
+    }
+
+}
