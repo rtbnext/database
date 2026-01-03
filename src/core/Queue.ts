04f8a0b44a4ba6b7abb24bdd79831f9bff10e927
komed3 <pk97@outlook.de>
2026-01-03 19:56:04 +0100

    implement basic queue operations

--- src/core/Queue.ts
+++ src/core/Queue.ts
@@ -1,6 +1,8 @@
+import { Config } from '@/core/Config';
+import { Utils } from '@/core/Utils';
 import { TQueueConfig } from '@/types/config';
-import { TQueue, TQueueType } from '@/types/queue';
-import { Config } from './Config';
+import { TQueue, TQueueItem, TQueueType } from '@/types/queue';
+import { sha256 } from 'js-sha256';
 
 abstract class Queue {
 
@@ -12,6 +14,35 @@ abstract class Queue {
     protected constructor ( type: TQueueType ) {
         this.queueType = type;
         this.queue = new Map();
+    }
+
+    private key ( uri: string, args?: any ) : string {
+        return sha256( uri + JSON.stringify( args ) );
+    }
+
+    public getQueue () : TQueueItem[] {
+        return Array.from( this.queue.values() );
+    }
+
+    public size () : number {
+        return this.queue.size;
+    }
+
+    public getByKey ( key: string ) : TQueueItem | undefined {
+        return this.queue.get( key );
+    }
+
+    public hasKey ( key: string ) : boolean {
+        return this.queue.has( key );
+    }
+
+    public getByUri ( uriLike: string ) : TQueueItem[] {
+        const uri = Utils.sanitize( uriLike );
+        return [ ...this.queue.values() ].filter( i => i.uri === uri );
+    }
+
+    public hasUri ( uriLike: string ) : boolean {
+        return this.getByUri( uriLike ).length !== 0;
     }
 
 }
