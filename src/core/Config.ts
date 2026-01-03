be5228e195a8a1ff236be2b1779de0b915c9da7d
komed3 <pk97@outlook.de>
2026-01-03 15:28:15 +0100

    describe config object type

--- src/core/Config.ts
+++ src/core/Config.ts
@@ -1,11 +1,14 @@
+import { TConfigObject } from '@/types/config';
 import { join } from 'node:path';
 import { cwd } from 'node:process';
 
 export class Config {
 
+    private static instance: Config;
     private readonly cwd: string;
     private readonly path: string;
     private readonly env: string;
+    private readonly config: TConfigObject;
 
     private constructor () {
         this.cwd = cwd();
