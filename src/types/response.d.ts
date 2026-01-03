32c4cfafeab600501b6b893d4ed4b13c501aea95
komed3 <pk97@outlook.de>
2026-01-03 15:59:56 +0100

    describe TWaybackResponse

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -6,3 +6,14 @@ export interface TResponse< T > {
     duration: number;
     retries: number;
 }
+
+export interface TWaybackResponse {
+    archived_snapshots: {
+        closest?: {
+            status: string;
+            available: boolean;
+            url: string;
+            timestamp: string;
+        };
+    };
+}
