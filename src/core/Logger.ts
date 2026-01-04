6e74524c9aaa8bd81fc957d083b4e345bb63d251
komed3 <pk97@outlook.de>
2026-01-04 10:09:30 +0100

    add public logger methods

--- src/core/Logger.ts
+++ src/core/Logger.ts
@@ -3,6 +3,7 @@ import { Utils } from '@/core/Utils';
 import { TLoggingConfig } from '@/types/config';
 import { appendFileSync, mkdirSync } from 'node:fs';
 import { join } from 'node:path';
+import { exit } from 'node:process';
 
 export class Logger {
 
@@ -19,10 +20,6 @@ export class Logger {
         this.config = logging;
         this.path = join( root, 'logs' );
         mkdirSync( this.path, { recursive: true } );
-    }
-
-    public static getInstance () : Logger {
-        return Logger.instance ||= new Logger();
     }
 
     private shouldLog ( level: TLoggingConfig[ 'level' ] ) : boolean {
@@ -50,6 +47,30 @@ export class Logger {
         const entry = this.format( level, msg, meta );
         if ( this.config.console ) this.log2Console( level, entry );
         if ( this.config.file ) this.log2File( entry );
+    }
+
+    public error ( msg: string, error?: Error ) : void {
+        this.log( 'error', msg, error );
+    }
+
+    public exit ( msg: string, error?: Error ) : never {
+        this.log( 'error', msg, error ); exit( 1 );
+    }
+
+    public warn ( msg: string, meta?: any ) : void {
+        this.log( 'warn', msg, meta );
+    }
+
+    public info ( msg: string, meta?: any ) : void {
+        this.log( 'info', msg, meta );
+    }
+
+    public debug ( msg: string, meta?: any ) : void {
+        this.log( 'debug', msg, meta );
+    }
+
+    public static getInstance () : Logger {
+        return Logger.instance ||= new Logger();
     }
 
 }
