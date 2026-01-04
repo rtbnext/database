6d91b290b469c2850bc790d29ef35e461741237c
komed3 <pk97@outlook.de>
2026-01-04 15:32:13 +0100

    Update storage.d.ts

--- src/interfaces/storage.d.ts
+++ src/interfaces/storage.d.ts
@@ -1,15 +1,15 @@
 export interface IStorage {
-    public exists ( path: string ) : boolean;
-    public assertPath ( path: string ) : void | never;
-    public ensurePath ( path: string, isDir: boolean = false ) : void;
-    public scanDir ( path: string, ext: string[] = [ 'json', 'csv' ] ) : string[];
-    public readJSON< T > ( path: string ) : T | false;
-    public writeJSON< T > ( path: string, content: T ) : boolean;
-    public readCSV< T extends any[] > ( path: string ) : T | false;
-    public writeCSV< T extends any[] > ( path: string, content: T ) : boolean;
-    public appendCSV< T extends any[] > ( path: string, content: T, nl: boolean = true ) : boolean;
-    public datedCSV< T extends any[] > ( path: string, content: T, force: boolean = false ) : boolean;
-    public move ( from: string, to: string, force: boolean = false ) : boolean;
-    public remove ( path: string, force: boolean = true ) : boolean;
-    public initDB () : void;
+    exists ( path: string ) : boolean;
+    assertPath ( path: string ) : void | never;
+    ensurePath ( path: string, isDir: boolean = false ) : void;
+    scanDir ( path: string, ext: string[] = [ 'json', 'csv' ] ) : string[];
+    readJSON< T > ( path: string ) : T | false;
+    writeJSON< T > ( path: string, content: T ) : boolean;
+    readCSV< T extends any[] > ( path: string ) : T | false;
+    writeCSV< T extends any[] > ( path: string, content: T ) : boolean;
+    appendCSV< T extends any[] > ( path: string, content: T, nl: boolean = true ) : boolean;
+    datedCSV< T extends any[] > ( path: string, content: T, force: boolean = false ) : boolean;
+    move ( from: string, to: string, force: boolean = false ) : boolean;
+    remove ( path: string, force: boolean = true ) : boolean;
+    initDB () : void;
 }
