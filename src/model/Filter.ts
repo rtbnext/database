06bbbe26959f8573ea5e3492dc9415e2b0934273
komed3 <pk97@outlook.de>
2026-01-05 19:06:41 +0100

    instantiate filter class

--- src/model/Filter.ts
+++ src/model/Filter.ts
@@ -1,3 +1,15 @@
+import { Storage } from '@/core/Storage';
 import { IFilter } from '@/interfaces/filter';
 
-export class Filter implements IFilter {}
+export class Filter implements IFilter {
+
+    private static readonly storage = Storage.getInstance();
+    private static instance: Filter;
+
+    private constructor () {}
+
+    public static getInstance () : Filter {
+        return Filter.instance ||= new Filter();
+    }
+
+}
