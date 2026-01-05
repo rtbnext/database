a8818ce217f9d6f65c6cad9744e76fd09649a18a
komed3 <pk97@outlook.de>
2026-01-05 10:00:09 +0100

    Update types/index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,13 @@
 import { IConfig } from '@/interfaces/config';
 import { IFetch } from '@/interfaces/fetch';
+import { IListIndex, IProfileIndex } from '@/interfaces/index';
 import { IQueue } from '@/interfaces/queue';
 import { IStorage } from '@/interfaces/storage';
+
+export interface TModel {
+    profileIndex: IProfileIndex;
+    listIndex: IListIndex;
+}
 
 export interface TServices {
     readonly config: IConfig;
