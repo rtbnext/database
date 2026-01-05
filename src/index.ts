62b4ec8f6986980a25b9ace2671a5a06b71f94e2
komed3 <pk97@outlook.de>
2026-01-05 09:59:00 +0100

    export indexes (profile/list)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { Config } from '@/core/Config';
 import { Fetch } from '@/core/Fetch';
 import { ListQueue, ProfileQueue } from '@/core/Queue';
 import { Storage } from '@/core/Storage';
-import { TServices } from '@/types/index';
+import { ProfileIndex } from '@/model/ProfileIndex';
+import { ListIndex } from '@/model/ListIndex';
+import { TModel, TServices } from '@/types/index';
 
 export * as Const from '@/core/Const';
 export { log } from '@/core/Logger';
@@ -13,4 +15,9 @@ export const services: TServices = {
     profileQueue: ProfileQueue.getInstance(),
     listQueue: ListQueue.getInstance(),
     storage: Storage.getInstance()
+};
+
+export const model: TModel = {
+    profileIndex: ProfileIndex.getInstance(),
+    listIndex: ListIndex.getInstance()
 };
