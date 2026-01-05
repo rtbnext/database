071718d278329b3a51c357af439f9a4058123292
komed3 <pk97@outlook.de>
2026-01-05 19:49:04 +0100

    describe IFilter

--- src/interfaces/filter.d.ts
+++ src/interfaces/filter.d.ts
@@ -1,1 +1,11 @@
-export interface IFilter {}
+import { TFilterGroup, TFilterSpecial } from '@rtbnext/schema/src/abstract/const';
+import { TFilter } from '@rtbnext/schema/src/model/filter';
+
+export interface IFilter {
+    getFilter ( group: TFilterGroup, key: string ) : TFilter[] | false;
+    getFilterByPath ( path: string ) : TFilter[] | false;
+    getGroup ( group: TFilterGroup ) : Record< string, TFilter[] >;
+    getSpecial ( special: TFilterSpecial ) : TFilter[];
+    has ( path: string, uriLike: string ) : boolean;
+    save ( collection: Partial< TFilterCollection > ) : void;
+}
