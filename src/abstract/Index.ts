cd5e6e16603103b6f469219b4484725da8d7371e
komed3 <pk97@outlook.de>
2026-01-05 09:42:11 +0100

    implement index updater

--- src/abstract/Index.ts
+++ src/abstract/Index.ts
@@ -3,16 +3,19 @@ import { Storage } from '@/core/Storage';
 import { Utils } from '@/core/Utils';
 import { IIndex } from '@/interfaces/index';
 import { TIndex } from '@rtbnext/schema/src/abstract/generic';
+import deepmerge from 'deepmerge';
 
 export abstract class Index<
     I extends TIndex, T extends Map< string, I >
 > implements IIndex< I, T > {
 
     protected static readonly storage = Storage.getInstance();
+    protected readonly type: 'profile' | 'list';
     protected readonly path: string;
     protected index: T;
 
-    protected constructor ( path: string ) {
+    protected constructor ( type: 'profile' | 'list', path: string ) {
+        this.type = type;
         this.path = path;
         Index.storage.ensurePath( this.path );
         this.index = this.loadIndex();
@@ -22,7 +25,7 @@ export abstract class Index<
 
     protected loadIndex () : T {
         const raw = Index.storage.readJSON< Record< string, I > > ( this.path ) ?? {};
-        log.debug( `Index loaded: ${ Object.keys( raw ).length } items from ${this.path}` );
+        log.debug( `Index [${this.type}] loaded: ${ Object.keys( raw ).length } items` );
         return new Map( Object.entries( raw ) ) as T;
     }
 
@@ -47,6 +50,28 @@ export abstract class Index<
 
     public get ( uriLike: string ) : I | undefined {
         return this.index.get( Utils.sanitize( uriLike ) );
+    }
+
+    // Manipulate index (add, update, remove items)
+
+    public update (
+        uriLike: string, data: Partial< I >, allowUpdate: boolean = true,
+        save: boolean = true
+    ) : I | false {
+        return log.catch( () => {
+            const uri = Utils.sanitize( uriLike );
+            if ( ! allowUpdate && this.index.has( uri ) ) return false;
+
+            log.debug( `Updating index [${this.type}] item: ${uri}` );
+            const item = deepmerge< I >( this.index.get( uri ) ?? {}, data, {
+                arrayMerge: ( t, s ) => Utils.mergeArray( t, s, 'unique' )
+            } );
+
+            this.index.set( uri, item );
+            if ( save ) this.saveIndex();
+
+            return item;
+        }, `Failed to update index [${this.type}] item: ${uriLike}` ) ?? false;
     }
 
 }
--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -7,7 +7,7 @@ export interface IIndex< I extends TIndex, T extends Map< string, I > > {
     size () : number;
     has ( uriLike: string ) : boolean;
     get ( uriLike: string ) : I | undefined;
-    update ( uriLike: string, data: Partial< I >, allowUpdate: boolean = true ) : I | false;
+    update ( uriLike: string, data: Partial< I >, allowUpdate: boolean = true, save: boolean = true ) : I | false;
     delta ( items: { uriLike: string, data: Partial< I > }[], allowUpdate: boolean = true ) : number;
     add ( uriLike: string, data: I ) : I | false;
     delete ( uriLike: string ) : void;
