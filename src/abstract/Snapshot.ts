d394fe03d0d1cdd48c60b168bedc3ad4ab75c075
komed3 <pk97@outlook.de>
2026-01-06 15:46:38 +0100

    implement basic snapshot getters

--- src/abstract/Snapshot.ts
+++ src/abstract/Snapshot.ts
@@ -1,6 +1,7 @@
 import { Storage } from '@/core/Storage';
 import { Utils } from '@/core/Utils';
 import { ISnapshot } from '@/interfaces/snapshot';
+import { Parser } from '@/parser/Parser';
 import { TSnapshot } from '@rtbnext/schema/src/abstract/generic';
 
 export abstract class Snapshot< T extends TSnapshot > implements ISnapshot< T > {
@@ -16,8 +17,28 @@ export abstract class Snapshot< T extends TSnapshot > implements ISnapshot< T > 
         this.dates = this.scanDates();
     }
 
+    // Private helpers
+
     protected scanDates () : string[] {
         return Utils.sort( Snapshot.storage.scanDir( this.path ) );
+    }
+
+    // Basic snapshot getters
+
+    public getDates () : string[] {
+        return this.dates;
+    }
+
+    public hasDate ( dateLike: string ) : boolean {
+        return this.dates.includes( Parser.date( dateLike, 'ymd' )! );
+    }
+
+    public firstDate () : string | undefined {
+        return this.dates[ 0 ];
+    }
+
+    public latestDate () : string | undefined {
+        return this.dates.at( -1 );
     }
 
 }
