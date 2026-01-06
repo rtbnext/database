0f76ad4cf69bf8e815c6d346d0b0ab43bc365198
komed3 <pk97@outlook.de>
2026-01-06 19:14:39 +0100

    update list class

--- src/model/List.ts
+++ src/model/List.ts
@@ -1,13 +1,21 @@
 import { Snapshot } from '@/abstract/Snapshot';
 import { IList } from '@/interfaces/list';
-import { TListSnapshot } from '@rtbnext/schema/src/model/list';
+import { ListIndex } from '@/model/ListIndex';
+import { TListIndexItem, TListSnapshot } from '@rtbnext/schema/src/model/list';
 
 export class List extends Snapshot< TListSnapshot > implements IList {
 
-    private static instance: List;
+    private static readonly index = ListIndex.getInstance();
 
-    private constructor () {
+    private readonly uri: string;
+    private data: TListIndexItem;
+
+    private constructor ( item?: TListIndexItem ) {
+        if ( ! item ) throw new Error( `List index item not given` );
+
         super( 'list', 'json' );
+        this.uri = item.uri;
+        this.data = item;
     }
 
 }
