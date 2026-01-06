e706ffea00e1a19bf44f54dff3d5663c6ad6e5bf
komed3 <pk97@outlook.de>
2026-01-06 19:09:53 +0100

    Update List.ts

--- src/model/List.ts
+++ src/model/List.ts
@@ -2,4 +2,12 @@ import { Snapshot } from '@/abstract/Snapshot';
 import { IList } from '@/interfaces/list';
 import { TListSnapshot } from '@rtbnext/schema/src/model/list';
 
-export class List extends Snapshot< TListSnapshot > implements IList {}
+export class List extends Snapshot< TListSnapshot > implements IList {
+
+    private static instance: List;
+
+    private constructor () {
+        super( 'list', 'json' );
+    }
+
+}
