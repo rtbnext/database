9bd79490d055da8062e10dd22bc3a154655292a4
komed3 <pk97@outlook.de>
2026-01-07 10:25:14 +0100

    Update profile.d.ts

--- src/interfaces/profile.d.ts
+++ src/interfaces/profile.d.ts
@@ -1,5 +1,5 @@
 import { TMetaData } from '@rtbnext/schema/src/abstract/generic';
-import { TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
+import { TProfileData, TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
 
 export interface IProfile {
     getUri () : string;
@@ -8,4 +8,10 @@ export interface IProfile {
     schemaVersion () : number;
     modified () : string;
     modifiedTime () : number;
+    getData () : TProfileData;
+    setData ( data: TProfileData, aliases?: string[] ) : void;
+    updateData (
+        data: Partial< TProfileData >, aliases?: string[],
+        mode: 'concat' | 'replace' | 'unique' = 'replace'
+    ) : void;
 }
