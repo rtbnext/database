741af23b556fbfbe8f1bac428fa3c186e6966de4
komed3 <pk97@outlook.de>
2026-01-07 09:53:15 +0100

    implement private constructor

--- src/model/Profile.ts
+++ src/model/Profile.ts
@@ -1,10 +1,30 @@
 import { Storage } from '@/core/Storage';
 import { IProfile } from '@/interfaces/profile';
 import { ProfileIndex } from '@/model/ProfileIndex';
+import { TMetaData } from '@rtbnext/schema/src/abstract/generic';
+import { TProfileData, TProfileHistory, TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
+import { join } from 'node:path';
 
 export class Profile implements IProfile {
 
     private static readonly storage = Storage.getInstance();
     private static readonly index = ProfileIndex.getInstance();
+
+    private uri: string;
+    private path: string;
+    private item: TProfileIndexItem;
+    private data?: TProfileData;
+    private history?: TProfileHistory;
+    private meta?: TMetaData;
+
+    private constructor ( item?: TProfileIndexItem ) {
+        if ( ! item ) throw new Error( `Profile index item not given` );
+
+        this.uri = item.uri;
+        this.path = join( 'profile', item.uri );
+        this.item = item;
+
+        Profile.storage.ensurePath( this.path, true );
+    }
 
 }
