5a2c2a44dc805de75dd649d6ecee71244b4e55ef
komed3 <pk97@outlook.de>
2026-01-07 10:07:50 +0100

    describe profile getters in IProfile

--- src/interfaces/profile.d.ts
+++ src/interfaces/profile.d.ts
@@ -1,1 +1,11 @@
-export interface IProfile {}
+import { TMetaData } from '@rtbnext/schema/src/abstract/generic';
+import { TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
+
+export interface IProfile {
+    getUri () : string;
+    getItem () : TProfileIndexItem;
+    getMeta () : TMetaData[ '@metadata' ];
+    schemaVersion () : number;
+    modified () : string;
+    modifiedTime () : number;
+}
