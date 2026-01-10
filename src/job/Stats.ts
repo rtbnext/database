e6d821e52a6f6ee59f393f0254cf1c1671c301fe
komed3 <pk97@outlook.de>
2026-01-10 11:04:28 +0100

    instantiate stats job

--- src/job/Stats.ts
+++ src/job/Stats.ts
@@ -1,4 +1,10 @@
 import { Job } from '@/abstract/Job';
 import { IJob } from '@/interfaces/job';
 
-export class StatsJob extends Job implements IJob {}
+export class StatsJob extends Job implements IJob {
+
+    constructor ( args: string[] ) {
+        super( args, 'Stats' );
+    }
+
+}
