2f5fe619d013561b1fe4a49f8b60836d722f1860
komed3 <pk97@outlook.de>
2026-01-10 17:25:31 +0100

    aggregate filter / stats data

--- src/job/Stats.ts
+++ src/job/Stats.ts
@@ -1,5 +1,9 @@
 import { Job, jobRunner } from '@/abstract/Job';
 import { IJob } from '@/interfaces/job';
+import { Filter } from '@/model/Filter';
+import { Profile } from '@/model/Profile';
+import { ProfileIndex } from '@/model/ProfileIndex';
+import { Stats } from '@/model/Stats';
 
 export class StatsJob extends Job implements IJob {
 
@@ -8,7 +12,22 @@ export class StatsJob extends Job implements IJob {
     }
 
     public async run () : Promise< void > {
-        await this.protect( async () => {} );
+        await this.protect( async () => {
+            const date = Job.stats.getGlobalStats().date;
+            const index = ProfileIndex.getInstance().getIndex();
+            if ( ! date || ! index.size ) throw new Error( `No data available` );
+
+            let filter = {}, stats = {};
+
+            for ( const item of index.values() ) {
+                const profile = Profile.getByItem( item );
+                if ( ! profile ) continue;
+
+                const data = profile.getData();
+                Stats.aggregate( data, date, stats );
+                Filter.aggregate( data, filter );
+            }
+        } );
     }
 
 }
