82c8ff112ab79c654bb8a01765b6fc3b57392313
komed3 <pk97@outlook.de>
2026-01-11 17:52:33 +0100

    implement wiki job runner

--- src/job/Wiki.ts
+++ src/job/Wiki.ts
@@ -1,5 +1,7 @@
 import { Job, jobRunner } from '@/abstract/Job';
 import { IJob } from '@/interfaces/job';
+import { Profile } from '@/model/Profile';
+import { Parser } from '@/parser/Parser';
 
 export class WikiJob extends Job implements IJob {
 
@@ -7,7 +9,18 @@ export class WikiJob extends Job implements IJob {
         super( args, 'Wiki' );
     }
 
-    public async run () : Promise< void > {}
+    public async run () : Promise< void > {
+        await this.protect( async () => {
+            const profile = Profile.find( Parser.strict( this.args.profile ?? '', 'string' )! );
+            if ( ! profile ) throw new Error( `Profile not found: ${this.args.profile}` );
+
+            if ( this.truthy( this.args.update ) ) await this.update( profile );
+            else if ( typeof this.args.assign === 'string' ) await this.assign(
+                profile, this.args.assign
+            );
+            else throw new Error( `No action specified` );
+        } );
+    }
 
 }
 
