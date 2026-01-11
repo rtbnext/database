85b311705ee9228e0e3d5c291505ef2800205784
komed3 <pk97@outlook.de>
2026-01-11 13:26:03 +0100

    securing image fetcher + logging

--- src/core/Wiki.ts
+++ src/core/Wiki.ts
@@ -1,4 +1,5 @@
 import { Fetch } from '@/core/Fetch';
+import { log } from '@/core/Logger';
 import { Parser } from '@/parser/Parser';
 import { TCommonsResponse } from '@/types/response';
 import { TImage } from '@rtbnext/schema/src/abstract/generic';
@@ -8,31 +9,35 @@ export class Wiki {
     private static readonly fetch = Fetch.getInstance();
 
     public static async queryCommonsImage ( title: string ) : Promise< TImage | undefined > {
-        const res = await Wiki.fetch.commons< TCommonsResponse >( {
-            action: 'query', titles: `File:${title}`, prop: 'imageinfo', redirects: 1,
-            iiprop: 'url|extmetadata', iiurlwidth: 400
-        } );
+        log.debug( `Querying Wikimedia Commons image: ${title}` );
+        return await log.catchAsync( async () => {
+            const res = await Wiki.fetch.commons< TCommonsResponse >( {
+                action: 'query', titles: `File:${title}`, prop: 'imageinfo', redirects: 1,
+                iiprop: 'url|extmetadata', iiurlwidth: 400
+            } );
 
-        const info = res.data?.query.pages?.[ 0 ]?.imageinfo?.[ 0 ];
-        if ( ! info ) return;
+            const info = res.data?.query.pages?.[ 0 ]?.imageinfo?.[ 0 ];
+            if ( ! info ) throw new Error( `No image info found for: ${title}` );
 
-        const meta = info.extmetadata ?? {};
-        const thumbUrl = info.thumburl ?? Object.values( info.responsiveUrls ?? {} ).at( 0 );
-        const dateTime = meta.DateTimeOriginal?.value ?? meta.DateTime?.value;
-        const credits = Parser.list( [
-            meta.Attribution?.value || meta.Artist?.value || meta.Credit?.value,
-            meta.LicenseShortName?.value || meta.UsageTerms?.value,
-            'via Wikimedia Commons'
-        ] ).join( ', ' );
+            log.debug( `Wikimedia Commons image info received for: ${title}` );
+            const meta = info.extmetadata ?? {};
+            const thumbUrl = info.thumburl ?? Object.values( info.responsiveUrls ?? {} ).at( 0 );
+            const dateTime = meta.DateTimeOriginal?.value ?? meta.DateTime?.value;
+            const credits = Parser.list( [
+                meta.Attribution?.value || meta.Artist?.value || meta.Credit?.value,
+                meta.LicenseShortName?.value || meta.UsageTerms?.value,
+                'via Wikimedia Commons'
+            ] ).join( ', ' );
 
-        return Parser.container< TImage >( {
-            url: { value: info.descriptionurl, type: 'string' },
-            file: { value: info.url, type: 'string' },
-            thumb: { value: thumbUrl, type: 'string' },
-            caption: { value: meta.ImageDescription?.value, type: 'string' },
-            date: { value: dateTime, type: 'date', args: [ 'iso' ] },
-            credits: { value: credits, type: 'safeStr' }
-        } );
+            return Parser.container< TImage >( {
+                url: { value: info.descriptionurl, type: 'string' },
+                file: { value: info.url, type: 'string' },
+                thumb: { value: thumbUrl, type: 'string' },
+                caption: { value: meta.ImageDescription?.value, type: 'string' },
+                date: { value: dateTime, type: 'date', args: [ 'iso' ] },
+                credits: { value: credits, type: 'safeStr' }
+            } );
+        }, `Failed to query Wikimedia Commons image: ${title}` );
     }
 
 }
