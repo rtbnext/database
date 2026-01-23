bf64943e11d60e2373dc60833bc27dabca00b008
komed3 <pk97@outlook.de>
2026-01-23 11:02:26 +0100

    implement alias + name methods

--- src/parser/ProfileParser.ts
+++ src/parser/ProfileParser.ts
@@ -28,4 +28,24 @@ export class ProfileParser implements IProfileParser {
         return this.cache( 'id', () => Utils.hash( this.raw.naturalId ) );
     }
 
+    public aliases () : string[] {
+        return this.cache( 'aliases', () => {
+            const uri = this.uri();
+            return this.raw.uris
+                .filter( Boolean )
+                .map( i => Utils.sanitize( i ) )
+                .filter( i => i !== uri )
+                .sort();
+        } );
+    }
+
+    public name () : ReturnType< typeof ProfileParser.name > {
+        return this.cache( 'name', () => ProfileParser.name(
+            this.raw.name, this.raw.lastName, this.raw.firstName,
+            Parser.boolean( this.raw.asianFormat )
+        ) );
+    }
+
+    public static name () {}
+
 }
