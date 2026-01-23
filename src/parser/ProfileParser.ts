2f24b43ef1bfc703b0438bcb69ec50f287b3fe47
komed3 <pk97@outlook.de>
2026-01-23 10:51:53 +0100

    instantiate profile parser

--- src/parser/ProfileParser.ts
+++ src/parser/ProfileParser.ts
@@ -1,1 +1,15 @@
-export class ProfileParser {}
+import { TProfileResponse } from '@/types/response';
+
+export class ProfileParser {
+
+    private readonly raw: TProfileResponse[ 'person' ];
+    private readonly lists: TProfileResponse[ 'person' ][ 'personLists' ];
+
+    constructor ( res: TProfileResponse ) {
+        this.raw = res.person;
+        this.lists = res.person.personLists.sort(
+            ( a, b ) => Number( b.date ?? 0 ) - Number( a.date ?? 0 )
+        );
+    }
+
+}
