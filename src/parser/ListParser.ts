a3cf9d4d221c888fe162c00e5b67100e5a2159b0
komed3 <pk97@outlook.de>
2026-01-24 16:06:54 +0100

    implement caching

--- src/parser/ListParser.ts
+++ src/parser/ListParser.ts
@@ -3,6 +3,18 @@ import { TListResponse } from '@/types/response';
 
 export class ListParser implements IListParser {
 
-    constructor ( private readonly raw: TListResponse[ 'personList' ][ 'personsLists' ][ number ] ) {}
+    private readonly raw: TListResponse[ 'personList' ][ 'personsLists' ][ number ];
+    private cachedData: Map< string, any > = new Map();
+
+    constructor ( raw: TListResponse[ 'personList' ][ 'personsLists' ][ number ] ) {
+        this.raw = raw;
+    }
+
+    // Caching
+
+    private cache< T = any > ( key: string, fn: () => T ) : T {
+        if ( ! this.cachedData.has( key ) ) this.cachedData.set( key, fn() );
+        return this.cachedData.get( key );
+    }
 
 }
