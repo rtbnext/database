ba0108afdc3a3755890ed62992e548ab0f04abf5
komed3 <pk97@outlook.de>
2026-01-24 16:50:50 +0100

    create Ranking.ts

---
+++ src/utils/Ranking.ts
@@ -0,0 +1,9 @@
+import { ListQueue } from '@/core/Queue';
+import { ListIndex } from '@/model/ListIndex';
+
+export class Ranking {
+
+    private static readonly index = ListIndex.getInstance();
+    private static readonly queue = ListQueue.getInstance();
+
+}
