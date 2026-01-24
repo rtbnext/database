ae356c47094e94f26dbc0dda6b3a55e12c76cf50
komed3 <pk97@outlook.de>
2026-01-24 16:05:35 +0100

    instantiate list parser

--- src/parser/ListParser.ts
+++ src/parser/ListParser.ts
@@ -1,3 +1,8 @@
 import { IListParser } from '@/interfaces/parser';
+import { TListResponse } from '@/types/response';
 
-export class ListParser implements IListParser {}
+export class ListParser implements IListParser {
+
+    constructor ( private readonly raw: TListResponse[ 'personList' ][ 'personsLists' ][ number ] ) {}
+
+}
