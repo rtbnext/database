5a7215bdcc5c30dd48e6ca3871ca7b223c8b660e
komed3 <pk97@outlook.de>
2026-01-24 14:42:09 +0100

    add static vars cmp and index to profile merger

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -1,12 +1,16 @@
 import { CmpStrAsync } from 'cmpstr';
 
 import { REGEX_URI_CLEANUP } from '@/core/RegEx';
+import { ProfileIndex } from '@/model/ProfileIndex';
 
 CmpStrAsync.filter.add( 'input', 'normalizeUri', ( uri: string ) : string =>
     uri.replace( REGEX_URI_CLEANUP, '' )
 );
 
 export class ProfileMerger {
+
+    private static readonly cmp = CmpStrAsync.create( { metric: 'dice', safeEmpty: true } );
+    private static readonly index = ProfileIndex.getInstance();
 
     // Prevent instantiation
 
