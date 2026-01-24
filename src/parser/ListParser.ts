24e7b6e0f6ca4c5ae10827da5b56340f9fb9a84f
komed3 <pk97@outlook.de>
2026-01-24 16:08:16 +0100

    get raw list entry data

--- src/parser/ListParser.ts
+++ src/parser/ListParser.ts
@@ -1,12 +1,14 @@
 import { IListParser } from '@/interfaces/parser';
 import { TListResponse } from '@/types/response';
 
+export type TListResponseEntry = TListResponse[ 'personList' ][ 'personsLists' ][ number ];
+
 export class ListParser implements IListParser {
 
-    private readonly raw: TListResponse[ 'personList' ][ 'personsLists' ][ number ];
+    private readonly raw: TListResponseEntry;
     private cachedData: Map< string, any > = new Map();
 
-    constructor ( raw: TListResponse[ 'personList' ][ 'personsLists' ][ number ] ) {
+    constructor ( raw: TListResponseEntry ) {
         this.raw = raw;
     }
 
@@ -15,6 +17,12 @@ export class ListParser implements IListParser {
     private cache< T = any > ( key: string, fn: () => T ) : T {
         if ( ! this.cachedData.has( key ) ) this.cachedData.set( key, fn() );
         return this.cachedData.get( key );
+    }
+
+    // Raw data
+
+    public rawData () : TListResponseEntry {
+        return this.raw;
     }
 
 }
