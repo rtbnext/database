6c2d2d4cfbec1da84392a8c2cc5650c2f3d7b1e2
komed3 <pk97@outlook.de>
2026-01-24 14:43:21 +0100

    search for similar URIs using CmpStr

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -1,4 +1,4 @@
-import { CmpStrAsync } from 'cmpstr';
+import { CmpStrAsync, CmpStrResult } from 'cmpstr';
 
 import { REGEX_URI_CLEANUP } from '@/core/RegEx';
 import { ProfileIndex } from '@/model/ProfileIndex';
@@ -11,6 +11,15 @@ export class ProfileMerger {
 
     private static readonly cmp = CmpStrAsync.create( { metric: 'dice', safeEmpty: true } );
     private static readonly index = ProfileIndex.getInstance();
+
+    private static similarURIs ( uri: string ) : string[] {
+        const revUri = uri.split( '-' ).reverse().join( '-' );
+        const keys = [ ...ProfileMerger.index.getIndex().keys() ];
+        return [ ...new Set( [
+            ...ProfileMerger.cmp.match< CmpStrResult[] >( keys, uri, 0.9 ).map( i => i.source ),
+            ...ProfileMerger.cmp.match< CmpStrResult[] >( keys, revUri, 0.8 ).map( i => i.source )
+        ] ) ];
+    }
 
     // Prevent instantiation
 
