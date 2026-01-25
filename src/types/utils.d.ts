0d830e54ec828a6a0403b0eff61153260d0cf78f
komed3 <pk97@outlook.de>
2026-01-25 11:28:26 +0100

    min fix

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -3,7 +3,7 @@ import { Profile } from '@/model/Profile';
 export type TProfileOperation = 'create' | 'update' | 'merge';
 
 export interface TProfileLookupResult {
-    profile: Profile | null;
+    profile: Profile | false;
     isExisting: boolean;
     isSimilar: boolean;
 }
