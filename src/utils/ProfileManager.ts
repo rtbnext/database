182fcf1f9ac0fb80e552240808298cafcf4ad7eb
komed3 <pk97@outlook.de>
2026-01-25 11:29:52 +0100

    implement static profile merger lookup method

--- src/utils/ProfileManager.ts
+++ src/utils/ProfileManager.ts
@@ -1,1 +1,17 @@
-export class ProfileManager {}
+import { TProfileData } from '@rtbnext/schema/src/model/profile';
+
+import { Profile } from '@/model/Profile';
+import { TProfileLookupResult } from '@/types/utils';
+import { ProfileMerger } from '@/utils/ProfileMerger';
+
+export class ProfileManager {
+
+    public static lookup ( uri: string, id: string, profileData: Partial< TProfileData > ) : TProfileLookupResult {
+        let profile = Profile.find( uri );
+        const isExisting = profile && profile.verify( id );
+        const isSimilar = ! isExisting && !! ( profile = ProfileMerger.findMatching( profileData )[ 0 ] );
+
+        return { profile, isExisting, isSimilar };
+    }
+
+}
