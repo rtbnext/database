c895d2994148969412959ab4d772fbb9badfc40f
komed3 <pk97@outlook.de>
2026-01-25 09:46:54 +0100

    implement private listMergeable() method

--- src/job/Merge.ts
+++ src/job/Merge.ts
@@ -1,10 +1,19 @@
 import { Job, jobRunner } from '@/abstract/Job';
 import { IJob } from '@/interfaces/job';
+import { ProfileMerger } from '@/utils/ProfileMerger';
 
 export class MergeJob extends Job implements IJob {
 
     constructor ( args: string[] ) {
         super( args, 'Merge' );
+    }
+
+    private listMergeable ( ...uriLike: any[] ) : void {
+        for ( const [ uri, list ] of Object.entries( ProfileMerger.listCandidates( uriLike ) ) ) {
+            console.log( `Candidates for ${uri}:` );
+            if ( ! list.length ) console.log( ' - None' );
+            for ( const candidate of list ) console.log( ` - ${candidate}` );
+        }
     }
 
     public async run () : Promise< void > {
