5f37f5423918f2687324499357c04366b0e60716
komed3 <pk97@outlook.de>
2026-01-25 09:38:45 +0100

    get profile names and check if source profile exists

--- src/job/Move.ts
+++ src/job/Move.ts
@@ -1,5 +1,7 @@
 import { Job, jobRunner } from '@/abstract/Job';
+import { Utils } from '@/core/Utils';
 import { IJob } from '@/interfaces/job';
+import { Profile } from '@/model/Profile';
 
 export class MoveJob extends Job implements IJob {
 
@@ -7,7 +9,16 @@ export class MoveJob extends Job implements IJob {
         super( args, 'Move' );
     }
 
-    public async run () : Promise< void > {}
+    public async run () : Promise< void > {
+        await this.protect( async () => {
+            const from = typeof this.args.from === 'string' && Utils.sanitize( this.args.from );
+            const to = typeof this.args.to === 'string' && Utils.sanitize( this.args.to );
+            if( ! from || ! to ) throw new Error( 'Invalid from/to profile names' );
+
+            const profile = Profile.find( from );
+            if ( ! profile ) throw new Error( `Profile ${from} not found` );
+        } );
+    }
 
 }
 
