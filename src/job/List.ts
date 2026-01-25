ef0b4fd21dd4876f62e6c3fee049aa0855f6ec76
komed3 <pk97@outlook.de>
2026-01-25 19:51:06 +0100

    add ListJob static vars

--- src/job/List.ts
+++ src/job/List.ts
@@ -1,7 +1,12 @@
 import { Job, jobRunner } from '@/abstract/Job';
+import { Fetch } from '@/core/Fetch';
+import { ListQueue } from '@/core/Queue';
 import { IJob } from '@/interfaces/job';
 
 export class ListJob extends Job implements IJob {
+
+    private static readonly fetch = Fetch.getInstance();
+    private static readonly queue = ListQueue.getInstance();
 
     constructor ( args: string[] ) {
         super( args, 'List' );
