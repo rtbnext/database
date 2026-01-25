a3c0cc1458a8207a489064872da1c58ea0e03ba8
komed3 <pk97@outlook.de>
2026-01-25 13:25:56 +0100

    get profile URIs for processing

--- src/job/Profile.ts
+++ src/job/Profile.ts
@@ -1,13 +1,24 @@
 import { Job, jobRunner } from '@/abstract/Job';
+import { ProfileQueue } from '@/core/Queue';
 import { IJob } from '@/interfaces/job';
+import { Parser } from '@/parser/Parser';
 
 export class ProfileJob extends Job implements IJob {
+
+    private static readonly queue = ProfileQueue.getInstance();
 
     constructor ( args: string[] ) {
         super( args, 'Profile' );
     }
 
-    public async run () : Promise< void > {}
+    public async run () : Promise< void > {
+        await this.protect( async () => {
+            const method = Parser.boolean( this.args.replace ) ? 'setData' : 'updateData';
+            const batch = 'profile' in this.args && typeof this.args.profile === 'string'
+                ? this.args.profile.split( ',' ).filter( Boolean )
+                : ProfileJob.queue.nextUri( Job.config.fetch.rateLimit.batchSize );
+        } );
+    }
 
 }
 
